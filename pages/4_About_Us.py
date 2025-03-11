5273fdef29e0735a295815192dd7b22425e4d78f
Darren Lo <132395951+drrnl@users.noreply.github.com>
2025-03-11 22:33:48 +0000

    Updating image paths

--- pages/4_About_Us.py
+++ pages/4_About_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 st.markdown("""
     <div style="border: 2px solid #4CAF50; padding: 10px; border-radius: 10px; width: 100%; margin : 0 auto; text-align: center;">
@@ -11,11 +12,11 @@ st.markdown("""
 st.markdown("<br><br>", unsafe_allow_html=True)
 
 team_members = [
-    {"name": "Eric Shen", "role":"Project Lead / Modeling", "image": "/Users/darren/Capstone210_Clinical/images/Eric.png", "caption": "Eric is the Project Lead"},
-    {"name": "Simone Ong", "role":"Project Manager / AWS Infastructure", "image": "/Users/darren/Capstone210_Clinical/images/Simone.png", "caption": "I’m excited to be able to help out as a PM for this project, while still being able to hone in on my technical modeling abilities and explore a new industry that I have little to no experience in. Being able to improve this process for patients from an outside perspective seems like a feat that is worthy and challenging."},
-    {"name": "Jonathan Luo", "role":"Project Manager / Data Engineer", "image": "/Users/darren/Capstone210_Clinical/images/Jonathan.jpeg", "caption": "I believe that getting the patient community closer to research advances healthcare for all. Through analytics and modeling, I strive to to enable a solution that effectively reforms a field that is people-centric and that I am passionate about."},
-    {"name": "Matthew Zhang", "role":"Modeling / AWS Infastructure", "image": "/Users/darren/Capstone210_Clinical/images/Matthew.png", "caption": "Eric is the Project Lead"},
-    {"name": "Darren Lo", "role":"Web Developement / Data Visualization", "image": "/Users/darren/Capstone210_Clinical/images/Darren.jpeg", "caption": "I'm thrilled to dive into web development for this clinical optimization matching project. It's an exciting challenge to apply my skills to improve healthcare processes through AI. I look forward to creating a seamless and impactful solution that enhances efficiency and outcomes."}]
+    {"name": "Eric Shen", "role":"Project Lead / Modeling", "image": os.path.join("images","Eric.png"), "caption": "Eric is the Project Lead"},
+    {"name": "Simone Ong", "role":"Project Manager / AWS Infastructure", "image": os.path.join("images","Simone.png"), "caption": "I’m excited to be able to help out as a PM for this project, while still being able to hone in on my technical modeling abilities and explore a new industry that I have little to no experience in. Being able to improve this process for patients from an outside perspective seems like a feat that is worthy and challenging."},
+    {"name": "Jonathan Luo", "role":"Project Manager / Data Engineer", "image": os.path.join("images","Jonathan.jpeg"), "caption": "I believe that getting the patient community closer to research advances healthcare for all. Through analytics and modeling, I strive to to enable a solution that effectively reforms a field that is people-centric and that I am passionate about."},
+    {"name": "Matthew Zhang", "role":"Modeling / AWS Infastructure", "image": os.path.join("images", "Matthew.png"), "caption": "Eric is the Project Lead"},
+    {"name": "Darren Lo", "role":"Web Developement / Data Visualization", "image": os.path.join("images","Darren.jpeg"), "caption": "I'm thrilled to dive into web development for this clinical optimization matching project. It's an exciting challenge to apply my skills to improve healthcare processes through AI. I look forward to creating a seamless and impactful solution that enhances efficiency and outcomes."}]
 
 
 
