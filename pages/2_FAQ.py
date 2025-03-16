84f9c32226e3e08be861791546e5ca947162bf98
Darren Lo <132395951+drrnl@users.noreply.github.com>
2025-03-16 19:59:16 +0000

    Fixed image into a container. FAQ updated to potential final format.

--- pages/2_FAQ.py
+++ pages/2_FAQ.py
@@ -3,14 +3,25 @@ import streamlit as st
 st.title("Frequently Asked Questions")
 
 # FAQ items and answers
-faqs = [
+generals = [
     {"question": "What is Clinical Optimization?", "answer": "Clinical optimization refers to improving healthcare processes to enhance efficiency, reduce costs, and improve patient outcomes."},
     {"question": "How does this project work?", "answer": "This project uses advanced algorithms to match patients with the most appropriate clinical interventions and treatments."},
-    {"question": "Who can use this tool?", "answer": "This tool is designed for healthcare professionals, administrators, and clinical researchers looking to optimize treatment protocols."},
-    {"question": "Is this tool free?", "answer": "The availability of this tool depends on the specific healthcare organization or project. Please check with your provider for access details."}
+    {"question": "Who can use this tool?", "answer": "This tool is designed for patients with ease of use in mind."},
+    {"question": "Is this tool free?", "answer": "Patients can sign up for free to use this tool."}
 ]
-
+models = [
+    {"question": "What are the models used in this product?", "answer": "Clinical optimization refers to improving healthcare processes to enhance efficiency, reduce costs, and improve patient outcomes."},
+    {"question": "How does the retrieval work?", "answer": "This project uses advanced algorithms to match patients with the most appropriate clinical interventions and treatments."},
+    {"question": "How are we ranking clinical trials suitable for you?", "answer": "This tool is designed for healthcare professionals, administrators, and clinical researchers looking to optimize treatment protocols."},
+    {"question": "What is the risk factor score?", "answer": "The availability of this tool depends on the specific healthcare organization or project. Please check with your provider for access details."}
+]
 # Create an expander for each FAQ
-for faq in faqs:
-    with st.expander(faq["question"]):
-        st.write(faq["answer"])
+st.subheader("General")
+for general in generals:
+    with st.expander(general["question"]):
+        st.write(general["answer"])
+
+st.subheader("Models")
+for model in models:
+    with st.expander(model["question"]):
+        st.write(model["answer"])
--- pages/3_Transparency.py
+++ pages/3_Transparency.py
@@ -1,16 +1,44 @@
 import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
 import os
 import base64
 
+    
 st.title("Transparency")
 
 st.subheader("Project Architecture")
-st.image(os.path.join("images","project_architecture.png"))
+
+def project_architecture():
+    with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                background-color: rgba(128, 128, 128, 0.5);  /* Grey with 50% transparency */
+                padding: 20px;
+                border-radius: 10px;  /* rounded corners */
+                display: inline-block;
+                width: 100%;  /* Ensure the container spans full width */
+                text-align: center;  /* Center the image */
+            }
+            """,
+    ):
+        st.image(os.path.join("images","project_architecture.png"))
+project_architecture()
+
 st.write("Some description of the architecture and then maybe bullet points about the individual models ")
 
 st.subheader("A W231 Collaboration - Ethics Review")
 st.write("Report written by: Joseph Chan")
 pdf_path = os.path.join("images","Capstone_Audit_by_Joseph_Chan.pdf")
+
+# Allow users to download the PDF
+with open(pdf_path, "rb") as file:
+    st.download_button(
+        label="Download PDF",
+        data=file,
+        file_name="EthicReview.pdf",
+        mime="application/pdf"
+    )
 
 def display_pdf(pdf_file):
     with open(pdf_file, "rb") as file:
@@ -23,15 +51,5 @@ def display_pdf(pdf_file):
     # Embed the PDF in the Streamlit app
     st.markdown(pdf_display, unsafe_allow_html=True)
 
-# Display the PDF in the app
-
 display_pdf(pdf_path)
 
-# Allow users to download the PDF
-with open(pdf_path, "rb") as file:
-    st.download_button(
-        label="Download PDF",
-        data=file,
-        file_name="EthicReview.pdf",
-        mime="application/pdf"
-    )
