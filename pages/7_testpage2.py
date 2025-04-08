d9007a9573be94e1ccbef77b4b5f2f2985bac395
Darren Lo <132395951+drrnl@users.noreply.github.com>
2025-04-08 03:29:31 +0000

    testing login

--- pages/7_testpage2.py
+++ pages/7_testpage2.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-if not st.experimental_user.is_logged_in:
-    if st.button("authenticate"):
-        st.login('google')
+if st.button("Authenticate"):
+    st.login("google")
+    st.login("google")
 
+st.json(st.experimental_user)
 
-if st.experimental_user.is_logged_in:
-    st.json(st.experimental_user)
-
-if st.button('Log Out'):
+if st.button('log out'):
     st.logout()
