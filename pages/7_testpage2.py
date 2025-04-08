ce59d5f1980037d1e5375f1e4e52ce2baa071bdf
Darren Lo <132395951+drrnl@users.noreply.github.com>
2025-04-08 03:09:30 +0000

    Added a security gate to Trial Matching

--- pages/7_testpage2.py
+++ pages/7_testpage2.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-if st.button("authenticate"):
-    st.login('google')
+if not st.experimental_user.is_logged_in:
+    if st.button("authenticate"):
+        st.login('google')
+
+
+if st.experimental_user.is_logged_in:
+    st.json(st.experimental_user)
+
+if st.button('Log Out'):
+    st.logout()
